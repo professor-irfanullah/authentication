78d49b700bb59112f0266a20ff762874a2668372
Professor Irfan Ullah <irfanprofessor60@gmail.com>
2025-05-07 16:10:16 +0500

    modifie loginUser.js according to postgreSql usage

--- src/routeHandlers/loginUser.js
+++ src/routeHandlers/loginUser.js
@@ -3,17 +3,17 @@ const { query } = require('../database/db')
 
 const LoginUser = async (req, res, next) => {
     const { email, password } = req.body
-    const Query = `select user_id , name , email, password , role from users where email = ? and is_verified = true`
+    const Query = `select user_id , name , email, password , role from users where email = $1 and is_verified = $2`
     try {
-        const response = await query(Query, [email])
-        if (response.length) {
+        const response = await query(Query, [email, true])
+        if (response.rows.length) {
             const payload = {
-                user_id: response[0].user_id,
-                name: response[0].name,
-                email: response[0].email,
-                role: response[0].role
+                user_id: response.rows[0].user_id,
+                name: response.rows[0].name,
+                email: response.rows[0].email,
+                role: response.rows[0].role
             }
-            const isValid = await verifyPassowrd(password, response[0].password)
+            const isValid = await verifyPassowrd(password, response.rows[0].password)
 
             if (isValid) {
                 const token = createToken(payload)
@@ -28,6 +28,7 @@ const LoginUser = async (req, res, next) => {
         const err = new Error('User not found!')
         err.status = 404
         next(err)
+
     } catch (error) {
 
         const err = new Error(error)
