31a08032c38630e51b0b7395f2bd150290a4d020
Professor Irfan Ullah <irfanprofessor60@gmail.com>
2025-05-08 17:52:57 +0500

    add to server.js

--- src/database/db.js
+++ src/database/db.js
@@ -1,13 +1,31 @@
 require('dotenv').config()
 const { Pool } = require('pg')
+// const db = new Pool({
+//     user: process.env.user,
+//     database: process.env.database,
+//     password: process.env.password,
+//     port: process.env.db_port || 5432
+// })
+
+// const db = new Pool({
+//     // connectionString: process.env.DATABASE_URL,
+//     connectionString: process.env.db_connection_url,
+
+// });
+
 const db = new Pool({
     user: process.env.user,
+    host: process.env.db_connection_url,  // Replace with your private domain
     database: process.env.database,
     password: process.env.password,
-    port: process.env.db_port || 5432,
-    ssl: false
-})
-
+    port: 5432,
+    max: 10, // Max number of connections
+    idleTimeoutMillis: 30000, // Idle timeout in milliseconds
+    connectionTimeoutMillis: 2000, // Time to wait for a connection to be established
+    ssl: {
+        rejectUnauthorized: false // Needed for some cloud providers like Railway
+    }
+});
 
 
 // Test the connection
