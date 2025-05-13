bebedc2770e5816f3f577110f2caeed478ea5152
Professor Irfan Ullah <irfanprofessor60@gmail.com>
2025-05-13 09:10:37 +0500

    update the profileUploadeHandler.js functionality

--- src/routeHandlers/profileUploadHandler.js
+++ src/routeHandlers/profileUploadHandler.js
@@ -3,22 +3,27 @@ const { query } = require('../database/db')
 const profileHandler = async (req, res, next) => {
     const { path } = req.file
     const user = req.user
+    const userCheckQuery = `select photo_url from seeker_profiles where user_id = $1`
     const insertQuery = `insert into seeker_profiles(user_id , photo_url) values($1, $2)`
     try {
-        const response = await uploadImageOnCloudinary(path)
-        const { secure_url } = response
-        const dbResponse = await query(insertQuery, [user.user_id, secure_url])
-        console.log(dbResponse);
+        const response = await query(userCheckQuery, [user.user_id])
 
+        if (response.rows.length) {
+            const err = new Error(`Profile picture already exists!`)
+            err.status = 403
+            return next(err)
+        }
+        const uploadResponse = await uploadImageOnCloudinary(path)
+        const { secure_url } = uploadResponse
+        await query(insertQuery, [user.user_id, secure_url])
         res.status(201).json({ url: secure_url, msg: 'profile photo successfully uploaded' })
     }
     catch (err) {
         if (err.code == '23505') {
-            const err = new Error(`user profile can only upload a single file!`)
+            const err = new Error(`user profile already exists!`)
             err.status = 401
             return next(err)
         }
-
         const error = new Error(err)
         next(error)
     }
