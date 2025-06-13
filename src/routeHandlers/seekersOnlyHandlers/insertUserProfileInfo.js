43362156a90e4eb674f6798f9c1fc4a343a0d3a2
Professor Irfan Ullah <irfanprofessor60@gmail.com>
2025-06-13 22:37:59 +0500

    change strict check to loose

--- src/routeHandlers/seekersOnlyHandlers/insertUserProfileInfo.js
+++ src/routeHandlers/seekersOnlyHandlers/insertUserProfileInfo.js
@@ -1,12 +1,12 @@
 const { query } = require('../../database/db')
 const insertUserProfileInfo = async (req, res, next) => {
     const checkQuery = `select profile_id from seeker_profiles where user_id = $1`
-    const updateQuery = `update seeker_profiles set headline = $1 , bio = $2 , location = $3 , phone = $4 , linkedin_url = $5 , github_url = $6 where user_id = $7`
-    const insertionQuery = `insert into seeker_profiles(user_id , headline , bio , location , phone ,linkedin_url ,github_url ) values($1,$2,$3,$4,$5, $6,$7)`
+    const updateQuery = `update seeker_profiles set headline = $1 , bio = $2 , location = $3  , linkedin_url = $4 , github_url = $5 where user_id = $6`
+    const insertionQuery = `insert into seeker_profiles(user_id , headline , bio , location  ,linkedin_url ,github_url ) values($1,$2,$3,$4,$5, $6)`
     const user = req.user
-    const { headline, bio, location, phone, linkedin_url, github_url } = req.body
+    const { headline, bio, location, linkedin_url, github_url } = req.body
 
-    if (!headline || !bio || !location || !phone || !linkedin_url || !github_url) {
+    if (headline == '' || bio == '' || location == '' || linkedin_url == '' || github_url == '') {
         const err = new Error(`All fields are required!`)
         err.status = 400
         return next(err)
@@ -14,13 +14,13 @@ const insertUserProfileInfo = async (req, res, next) => {
     try {
         const dbResponse = await query(checkQuery, [user.user_id])
         if (dbResponse.rows.length) {
-            const updateDBResponse = await query(updateQuery, [headline, bio, location, phone, linkedin_url, github_url, user.user_id])
+            const updateDBResponse = await query(updateQuery, [headline, bio, location, linkedin_url, github_url, user.user_id])
             if (updateDBResponse.rowCound === 1) {
                 return res.status(201).json({ msg: "operation successful" })
             }
             return res.status(200).json({ msg: 'operation successful' })
         }
-        const insertDBResponse = await query(insertionQuery, [user.user_id, headline, bio, location, phone, linkedin_url, github_url])
+        const insertDBResponse = await query(insertionQuery, [user.user_id, headline, bio, location, linkedin_url, github_url])
         res.send(insertDBResponse)
     } catch (error) {
         if (error.constraint === 'seeker_profiles_user_id_fkey') {
@@ -28,7 +28,8 @@ const insertUserProfileInfo = async (req, res, next) => {
             err.status = 404
             return next(err)
         }
-        res.status(500).json(error)
+        const err = new Error(error.message)
+        next(err)
     }
 }
 module.exports = { insertUserProfileInfo }
