a6b6fd22f5a6a551d35338270d8727fa9718d6eb
Professor Irfan Ullah <irfanprofessor60@gmail.com>
2025-08-15 19:03:23 +0500

    add employee router and imported all its required roles access

---
+++ src/routes/employe_route.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const router = express.Router()
+const { verifyToken } = require('../middlewares/verifyTokenMiddleware')
+const authorize = require('../middlewares/authorizationMiddleware')
+const { insertEmployeeProfile } = require('../routeHandlers/employer_profiles/insertEmployeeProfile')
+const { fetchProfile } = require('../routeHandlers/employer_profiles/fetchEmployeeProfile')
+const { fetchProfilePercentage } = require('../routeHandlers/employer_profiles/fetchEmployeeProfileCompletionStatus')
+const { fetchEmployeeAllJobs } = require('../routeHandlers/employer_profiles/fetchAllPostedJobByEmployee')
+const { postJob } = require('../routeHandlers/employer_profiles/insertJob')
+const { deleteJob } = require('../routeHandlers/employer_profiles/deleteJobPosting')
+const { updateJob } = require('../routeHandlers/employer_profiles/updateJobs')
+router.use(express.json())
+
+router.post('/insertProfile', verifyToken, authorize(['employee']), insertEmployeeProfile)
+router.post('/post/job', verifyToken, authorize(['employee']), postJob)
+router.post('/update/job', verifyToken, authorize(['employee']), updateJob)
+
+
+router.get('/fetchProfile', verifyToken, authorize(['employee']), fetchProfile)
+router.get('/fetchProfilePercentage', verifyToken, authorize(['employee']), fetchProfilePercentage)
+router.get('/fetchEmployeeAllJobs', verifyToken, authorize(['employee']), fetchEmployeeAllJobs)
+
+router.delete('/delete/job', verifyToken, authorize(['employee']), deleteJob)
+
+module.exports = router 
