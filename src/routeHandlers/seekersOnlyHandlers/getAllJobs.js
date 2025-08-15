dc367063f30a4b5021467f9529af481dcb8f24f3
Professor Irfan Ullah <irfanprofessor60@gmail.com>
2025-08-15 19:04:57 +0500

    improve query for fetching all jobs

--- src/routeHandlers/seekersOnlyHandlers/getAllJobs.js
+++ src/routeHandlers/seekersOnlyHandlers/getAllJobs.js
@@ -1,8 +1,7 @@
 const { query } = require("../../database/db")
 
 const allJobs = async (req, res, next) => {
-    const jobFetchQuery = `select * from jobs`
-
+    const jobFetchQuery = `select j.job_id , j.employer_id , j.title , j.description , j.requirements , j.location , j.employment_type , j.is_remote , j.salary_min , j.salary_max , j.status , j.posted_at , j.application_deadline , j.responsibilities , ep.company_name from jobs j left join employer_profiles ep on ep.profile_id = j.employer_id order by j.job_id desc`
     try {
         const response = await query(jobFetchQuery)
         res.status(200).json({ jobs: response.rows })
