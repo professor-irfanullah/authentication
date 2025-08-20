69b3b0961263aa54a485043739bc8f718728aef4
Professor Irfan Ullah <irfanprofessor60@gmail.com>
2025-08-20 11:32:12 +0500

    instead of local development i add neon database for hosting my database and add the logic of connecting to that

--- src/database/db.js
+++ src/database/db.js
@@ -1,13 +1,19 @@
 require('dotenv').config()
 const { Pool } = require('pg')
-const db = new Pool({
-    host: process.env.user,
-    user: process.env.pg_user,
-    database: process.env.database,
-    password: process.env.password,
-    port: process.env.db_port || 5432
-})
+// const db = new Pool({
+//     host: process.env.user,
+//     user: process.env.pg_user,
+//     database: process.env.database,
+//     password: process.env.password,
+//     port: process.env.db_port || 5432
+// })
 
+const db = new Pool({
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false
+    }
+});
 db.connect()
     .then(() => {
         console.log('Connected to PostgreSQL successfully!');
