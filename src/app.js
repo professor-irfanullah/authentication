09df7ba340f307d0293d5cad43c4ae475d49e275
Professor Irfan Ullah <irfanprofessor60@gmail.com>
2025-09-24 17:22:14 +0500

    add vercel domain to this

--- src/app.js
+++ src/app.js
@@ -8,7 +8,7 @@ const seekerRoutes = require('./routes/seekers_route')
 const employeeRoutes = require('../src/routes/employe_route')
 const app = express()
 const allowedOrigins = [
-    'https://job-board-front-end-psi.vercel.app',
+    'https://job-board-front-end-zr2b.vercel.app',
     'https://job-board-frontend-production.up.railway.app',
     'http://localhost:5173'
 ];
